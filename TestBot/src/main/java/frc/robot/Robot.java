6fdaab86fb848de2991f57a302b37337a0e7ec78
Creeon <92059871+Creeon@users.noreply.github.com>
2024-02-18 16:15:13 -0600

    Test code to test all functions.

--- TestBot/src/main/java/frc/robot/Robot.java
+++ TestBot/src/main/java/frc/robot/Robot.java
@@ -6,6 +6,7 @@ package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+
 /**
  * The VM is configured to automatically run this class, and to call the functions corresponding to
  * each mode, as described in the TimedRobot documentation. If you change the name of this class or
@@ -27,6 +28,7 @@ public class Robot extends TimedRobot {
   public void robotPeriodic() {
     CommandScheduler.getInstance().run();
   }
+
 
   @Override
   public void autonomousInit() {}
