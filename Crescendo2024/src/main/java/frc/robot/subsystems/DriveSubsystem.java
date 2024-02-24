3bd05d924fe46ad09a8b3a2440a313380428ca94
Ray <rayxia6@gmail.com>
2024-02-24 13:10:10 -0600

    Fix test

--- Crescendo2024/src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ Crescendo2024/src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -277,8 +277,6 @@ public class DriveSubsystem extends SubsystemBase {
   }
   @Override
   public void periodic() {
-
-    SmartDashboard.putNumber("Absolute Encoder", code.getAbsolutePosition().getValueAsDouble());
     // This method will be called once per scheduler run
 
     updateOdometry();
